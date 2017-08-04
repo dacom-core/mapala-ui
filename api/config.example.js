8ec16654a630185ed30d41c58da3e78e12748406
Tom Bogomolov <thedevelopertom@gmail.com>
2017-08-04 12:34:07 +0300

    close #5

--- api/config.example.js
+++ api/config.example.js
@@ -1,5 +1,4 @@
 export const JWT_AUTH_URL = '/configs-auth/'
 export const JWT_REFRESH_URL = '/configs-auth-refresh/'
-
-const LOGIN_URL = '/auth/login/'
-const SIGN_UP_URL = '/auth/sign_up/'
+export const LOGIN_URL = '/auth/login/'
+export const SIGN_UP_URL = '/auth/sign_up/'
