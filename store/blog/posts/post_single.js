e461f901b26e032a3096f73ca243db89df45d150
Tom Bogomolov <thedevelopertom@gmail.com>
2017-10-06 15:10:31 +0300

    post loading bugfix

--- store/blog/posts/post_single.js
+++ store/blog/posts/post_single.js
@@ -16,12 +16,15 @@ export const state = () => ({
     position_text: '',
     prev_page: null,
     title: ''
-  }
+  },
+  isLoading: false
 })
 
 export const actions = {
   async fetch_single_post ({ commit }, urlParams) {
+    commit('TOGGLE_POST_LOADING')
     const { data } = await Post.get({ permlink: urlParams.username + '*@*' + urlParams.slug })
+    commit('TOGGLE_POST_LOADING')
     commit('SET_POST_SINGLE', data)
   }
 }
@@ -30,10 +33,26 @@ export const mutations = {
   SET_POST_SINGLE (state, payload) {
     state.postSingle = payload
   },
+  RESET_POST_SINGLE (state) {
+    state.postSingle = false
+  },
   PUSH_NEW_COMMENT (state, comment) {
     state.postSingle.comments.push(comment)
   },
   SET_COMMENT (state, payload) {
     state.postSingle.comments = payload
+  },
+  TOGGLE_POST_LOADING (state) {
+    state.isLoading = !state.isLoading
+  }
+}
+
+export const getters = {
+
+  postMetaDescription (state) {
+    return state.postSingle.body ? state.postSingle.body.substr(0, 200) : 'Mapala.net user\'s post'
+  },
+  postMetaTitle (state) {
+    return state.postSingle.title ? state.postSingle.title : 'Mapala.net post'
   }
 }
