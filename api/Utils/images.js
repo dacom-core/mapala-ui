870a5741f03c70df6a5a5d4e2f0214cf2156189f
Tom Bogomolov <thedevelopertom@gmail.com>
2017-10-06 15:14:50 +0300

    code style images.js util

--- api/Utils/images.js
+++ api/Utils/images.js
@@ -6,40 +6,38 @@ import bc from '@/api/blockchain'
 
 import { Signature, PrivateKey, hash } from 'graphenejs-lib'
 
-
-
-export async function uploadImage(file) {
+export async function uploadImage (file) {
   const username = bc.current.blockchain_username
   const d = PrivateKey.fromWif(bc.current.wif)
 
-  if(!username) {
-      throw new Error('Please logged in first.')
+  if (!username) {
+    throw new Error('Please logged in first.')
   }
 
-  if(!d) {
-      throw new Error('Login with your posting key')
+  if (!d) {
+    throw new Error('Login with your posting key')
   }
 
-  if(!file && !dataUrl) {
-      console.error('uploadImage required: file or dataUrl')
+  if (!file && !dataUrl) {
+    console.error('uploadImage required: file or dataUrl')
   }
 
   let data, dataBs64
 
-  if(file) {
+  if (file) {
     const reader = new FileReader()
     data = await new Promise(resolve => {
-        reader.addEventListener('load', () => {
-            const result = new Buffer(reader.result, 'binary')
-            resolve(result)
-        })
-        reader.readAsBinaryString(file)
+      reader.addEventListener('load', () => {
+        const result = new Buffer(reader.result, 'binary')
+        resolve(result)
+      })
+      reader.readAsBinaryString(file)
     })
   } else {
-      // recover from preview
-      const commaIdx = dataUrl.indexOf(',')
-      dataBs64 = dataUrl.substring(commaIdx + 1)
-      data = new Buffer(dataBs64, 'base64')
+    // recover from preview
+    const commaIdx = dataUrl.indexOf(',')
+    dataBs64 = dataUrl.substring(commaIdx + 1)
+    data = new Buffer(dataBs64, 'base64')
   }
 
   // The challenge needs to be prefixed with a constant (both on the server and checked on the client) to make sure the server can't easily make the client sign a transaction doing something else.
@@ -49,12 +47,12 @@ export async function uploadImage(file) {
   const formData = new FormData()
 
   if(file) {
-      formData.append('file', file)
+    formData.append('file', file)
   } else {
-      // formData.append('file', file, filename) <- Failed to add filename=xxx to Content-Disposition
-      // Can't easily make this look like a file so this relies on the server supporting: filename and filebinary
-      formData.append('filename', filename)
-      formData.append('filebase64', dataBs64)
+    // formData.append('file', file, filename) <- Failed to add filename=xxx to Content-Disposition
+    // Can't easily make this look like a file so this relies on the server supporting: filename and filebinary
+    formData.append('filename', filename)
+    formData.append('filebase64', dataBs64)
   }
 
   const sig = Signature.signBufferSha256(bufSha, d)
